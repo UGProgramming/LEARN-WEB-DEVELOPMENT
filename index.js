a3d6e246130d3226e33475629524072ec89f8a1f
LearnWebDevelopment <148066457+UGProgramming@users.noreply.github.com>
2024-03-27 23:46:38 +0100

    loading project

---
+++ index.js
@@ -0,0 +1,13 @@
+
+ 
+    window.addEventListener("animationend", () => {
+    
+      document.querySelector('.loading-page').classList.add('loading-page--hidden')
+      document.querySelector('.loading-page').classList.add('spin')
+      
+}
+    
+    );
+
+    
+      
