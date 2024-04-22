c0d541aff4b2e78e8c18a7e18798912bc7fdf8c2
LearnWebDevelopment <148066457+UGProgramming@users.noreply.github.com>
2024-04-22 22:11:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ event.PreventDefault();
 window.close();
 
 });
+
+
+document.getElementById('continueButton').addEventListener('click', function() {
+    // Hide the hidden message
+    document.querySelector('.hidden-message').style.display = 'none';
+    // Display the body content
+    document.querySelector('body').style.display = 'block';
+});
